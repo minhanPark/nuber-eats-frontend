d19425a89ea9f0ace7687efce134ef0398fa2e4f
박민한 <runningwater@optatumplatform.com>
2021-01-26 10:23:01 +0900

    add .

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,44 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+export const LoggedOutRouter = () => {
+  const { register, watch, handleSubmit, errors } = useForm();
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log("can't create account");
+  };
+  console.log("errors", errors);
+  return (
+    <div>
+      <h1>Logged Out</h1>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: "this is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            ref={register({
+              required: true,
+            })}
+            name="password"
+            type="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <button className="bg-yellow-300 text">Submit</button>
+      </form>
+    </div>
+  );
+};
