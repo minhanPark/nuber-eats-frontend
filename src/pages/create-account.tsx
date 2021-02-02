b6f97400e0ab97e829ee16af64ff18037caa65c5
박민한 <runningwater@optatumplatform.com>
2021-02-02 13:23:17 +0900

    add header

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -45,7 +45,7 @@ export const CreateAccount = () => {
       createAccount: { ok },
     } = data;
     if (ok) {
-      history.push("/login");
+      history.push("/");
     }
   };
   const [
@@ -130,7 +130,7 @@ export const CreateAccount = () => {
         </form>
         <div>
           Already have ad account ?
-          <Link to="/login" className="text-lime-600 hover:underline">
+          <Link to="/" className="text-lime-600 hover:underline">
             Log in now
           </Link>
         </div>
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,21 +1,27 @@
-import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
 import { isLoggedInVar } from "../apollo";
-import { meQuery } from "../__generated__/meQuery";
+import { Header } from "../components/Header";
+import { useMe } from "../hooks/useMe";
+import { Restaurants } from "../pages/client/restaurants";
+import { ConfirmEmail } from "../pages/user/confirm-email";
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
+const ClientRoutes = [
+  <Route key={1} path="/" exact>
+    <Restaurants />
+  </Route>,
+  <Route key={2} path="/confirm" exact>
+    <ConfirmEmail />
+  </Route>,
+];
 
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  const { data, loading, error } = useMe();
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -25,8 +31,12 @@ export const LoggedInRouter = () => {
   }
 
   return (
-    <div>
-      <h1>{data.me.email}</h1>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        {data.me.role === "Client" && ClientRoutes}
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 };
