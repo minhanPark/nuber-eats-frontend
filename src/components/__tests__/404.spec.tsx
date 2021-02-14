1eb1b3090d432c8d5650c513679f60fec5115bbe
Running water <alsgks1218@naver.com>
2021-02-14 22:15:19 +0900

    it test

--- src/components/__tests__/404.spec.tsx
+++ src/components/__tests__/404.spec.tsx
@@ -1,17 +1,9 @@
-import { render, waitFor } from "@testing-library/react";
-import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter as Router } from "react-router-dom";
+import { render, waitFor } from "../../test-utils";
 import { NotFound } from "../../pages/404";
 
 describe("<NotFound />", () => {
   it("renders OK", async () => {
-    render(
-      <HelmetProvider>
-        <Router>
-          <NotFound />
-        </Router>
-      </HelmetProvider>
-    );
+    render(<NotFound />);
     await waitFor(() => {
       expect(document.title).toBe("Not Found | Nuber Eats");
     });
--- src/components/__tests__/login.spec.tsx
+++ src/components/__tests__/login.spec.tsx
@@ -1,16 +1,17 @@
 import { ApolloProvider } from "@apollo/client";
-import { createMockClient } from "mock-apollo-client";
+import { createMockClient, MockApolloClient } from "mock-apollo-client";
 import { render, RenderResult, waitFor } from "@testing-library/react";
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Login } from "../../pages/login";
+import { Login, LOGIN_MUTATION } from "../../pages/login";
 import userEvent from "@testing-library/user-event";
 
 describe("<Login />", () => {
   let renderResult: RenderResult;
+  let mockedClient: MockApolloClient;
   beforeEach(async () => {
     await waitFor(async () => {
-      const mockedClient = createMockClient();
+      mockedClient = createMockClient();
       renderResult = render(
         <HelmetProvider>
           <Router>
@@ -29,7 +30,7 @@ describe("<Login />", () => {
     });
   });
   it("displays email validation errors", async () => {
-    const { getByPlaceholderText, debug, getByRole } = renderResult;
+    const { getByPlaceholderText, getByRole } = renderResult;
     const email = getByPlaceholderText(/email/i);
     await waitFor(() => {
       userEvent.type(email, "this@wont");
@@ -39,6 +40,54 @@ describe("<Login />", () => {
     await waitFor(() => {
       userEvent.clear(email);
     });
-    debug();
+    errorMessage = getByRole("alert");
+    expect(errorMessage).toHaveTextContent(/email is required/i);
+  });
+  it("display password required errors", async () => {
+    const { getByPlaceholderText, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+    const submitBtn = getByRole("button");
+    await waitFor(() => {
+      userEvent.type(email, "this@wont.com");
+      userEvent.click(submitBtn);
+    });
+    const errorMessage = getByRole("alert");
+    expect(errorMessage).toHaveTextContent(/password is required/i);
+  });
+  it("submits form and calls mutation", async () => {
+    const { getByPlaceholderText, getByRole } = renderResult;
+    const email = getByPlaceholderText(/email/i);
+    const password = getByPlaceholderText(/password/i);
+    const submitBtn = getByRole("button");
+    const formData = {
+      email: "real@test.com",
+      password: "123123",
+    };
+    const mockedMutationResponse = jest.fn().mockResolvedValue({
+      data: {
+        login: {
+          ok: true,
+          token: "XXX",
+          error: "mutation-error",
+        },
+      },
+    });
+    mockedClient.setRequestHandler(LOGIN_MUTATION, mockedMutationResponse);
+    jest.spyOn(Storage.prototype, "setItem");
+    await waitFor(() => {
+      userEvent.type(email, formData.email);
+      userEvent.type(password, formData.password);
+      userEvent.click(submitBtn);
+    });
+    expect(mockedMutationResponse).toHaveBeenCalledTimes(1);
+    expect(mockedMutationResponse).toHaveBeenCalledWith({
+      loginInput: {
+        email: formData.email,
+        password: formData.password,
+      },
+    });
+    const errorMessage = getByRole("alert");
+    expect(errorMessage).toHaveTextContent(/mutation-error/i);
+    expect(localStorage.setItem).toHaveBeenCalledWith("nuber-token", "XXX");
   });
 });
